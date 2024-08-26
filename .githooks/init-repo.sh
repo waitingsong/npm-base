f7f435e01467bcec581b8c9f4df3fdc1287cf462
waiting <1661926154@qq.com>
2024-08-26 15:13:33 +0800

    chore: squash merge

--- .githooks/init-repo.sh
+++ .githooks/init-repo.sh
@@ -9,21 +9,22 @@ set -e
 
 git init
 git config --global i18n.commitencoding utf-8
+git config --global core.autocrlf input
+git config --global core.eol lf
 
 if [ -z "$CI" ]; then
-  git config --local core.autocrlf false
-  git config --local core.eol lf
-  git config --local core.filemode false
-  git config --local core.hooksPath ./.githooks
-  git config --local core.ignorecase false
-  git config --local core.precomposeUnicode true
-  git config --local fetch.prune true
-  git config --local pull.rebase true
-  git config --local push.followTags true
-  git config --local rebase.autoStash true
-  git config --local remote.origin.prune true
-  git config --local remote.origin.tagopt --tags
-  git config --local remote.pushdefault origin
+  git config core.filemode false
+  git config core.hooksPath ./.githooks
+  git config core.ignorecase false
+  git config core.precomposeUnicode true
+  git config fetch.prune true
+  git config pull.rebase true
+  git config push.autoSetupRemote true
+  git config push.followTags true
+  git config rebase.autoStash true
+  git config remote.origin.prune true
+  git config remote.origin.tagopt --tags
+  git config remote.pushdefault origin
 fi;
 
 echo It may going for a long time. Plese wait...
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
+#!/usr/bin/env tsx
+
 export const foo = 1
+console.info('hello world!')
+
